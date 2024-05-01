8dfd6b8021119a6115b983fc5cafd120c53e3dc8
Aakash Patel <aakash.patel0377@gmail.com>
2024-04-30 20:25:16 -0400

    changed secret keys

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,12 @@
+import os 
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
+"""
 SECRET_KEY = '\xfaR\x11\xa7p\x197\xf6\xf0\xf7\x1e\xadj\xc5\xde\xc4'
 CLIENT_ID = "094f4f45e8e94cd5b9ca03a7796b1fae"
 CLIENT_SECRET = "f9650f7c701f4aa1a8c0632eb3c02499"
 MONGODB_HOST = 'mongodb+srv://aakashpatel0377:mongoDBAakashMM@cluster0.tuubvqi.mongodb.net/cmsc388j_final_db?retryWrites=true&w=majority&appName=Cluster0'
-
-
+"""
